a3627b1d84fb98c53eec9dfdac2e48d5d89be9ea
elidacon <elidacon@student.21-school.ru>
2023-11-07 00:20:24 +0500

    Add part 3.1

---
+++ src/part3.sql
@@ -0,0 +1,39 @@
+-- @block
+-- @conn school21
+CREATE OR REPLACE FUNCTION fnc_transferred_points() RETURNS TABLE(
+        peer1 VARCHAR,
+        peer2 VARCHAR,
+        points_amount INTEGER
+    ) AS $$ WITH transfer_sum AS (
+        SELECT checking_peer,
+            checked_peer,
+            SUM(points_amount) AS points_amount,
+            -- min ID (to recognize the first appearance of a pair later)
+            MIN(id) AS id,
+            -- peers pairs (peer1-peer2 and peer2-peer1 will always have
+            -- peer1-peer2 pair)
+            CONCAT(
+                LEAST(checking_peer, checked_peer),
+                '_',
+                GREATEST(checking_peer, checked_peer)
+            ) AS pair
+        FROM transferred_points
+        GROUP BY checking_peer,
+            checked_peer
+    )
+SELECT t1.checking_peer,
+    t1.checked_peer,
+    t1.points_amount - COALESCE(t2.points_amount, 0) AS points_amounts
+FROM transfer_sum AS t1
+    INNER JOIN (
+        -- to remove the duplicates of forward and backward transfers
+        SELECT pair,
+            MIN(id) AS min_id
+        FROM transfer_sum
+        GROUP BY pair
+    ) AS first_pair ON t1.pair = first_pair.pair
+    AND t1.id = first_pair.min_id
+    LEFT JOIN transfer_sum AS t2 -- to subtract the amount of a backward transfer
+    ON t1.checking_peer = t2.checked_peer
+    AND t1.checked_peer = t2.checking_peer
+    AND t1.checked_peer = t2.checking_peer $$ LANGUAGE SQL;
