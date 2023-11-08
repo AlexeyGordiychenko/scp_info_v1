4ffbfb5583c0f92846f83bd043957474b8a187ef
elidacon <elidacon@student.21-school.ru>
2023-11-08 23:06:37 +0500

    Remove rows with 0 points from part 3.1
    Change formatting also

--- src/part3.sql
+++ src/part3.sql
@@ -8,8 +8,7 @@ OR REPLACE FUNCTION fnc_transferred_points() RETURNS TABLE(
     peer2 VARCHAR,
     points_amount INTEGER
 ) AS $$ WITH transfer_sum AS (
-    SELECT
-        checking_peer,
+    SELECT checking_peer,
         checked_peer,
         SUM(points_amount) AS points_amount,
         -- min ID (to recognize the first appearance of a pair later)
@@ -21,33 +20,27 @@ OR REPLACE FUNCTION fnc_transferred_points() RETURNS TABLE(
             '_',
             GREATEST(checking_peer, checked_peer)
         ) AS pair
-    FROM
-        transferred_points
-    GROUP BY
-        checking_peer,
+    FROM transferred_points
+    GROUP BY checking_peer,
         checked_peer
 )
-SELECT
-    t1.checking_peer,
+SELECT t1.checking_peer,
     t1.checked_peer,
     t1.points_amount - COALESCE(t2.points_amount, 0) AS points_amounts
-FROM
-    transfer_sum AS t1
+FROM transfer_sum AS t1
     INNER JOIN (
         -- to remove the duplicates of forward and backward transfers
-        SELECT
-            pair,
+        SELECT pair,
             MIN(id) AS min_id
-        FROM
-            transfer_sum
-        GROUP BY
-            pair
+        FROM transfer_sum
+        GROUP BY pair
     ) AS first_pair ON t1.pair = first_pair.pair
     AND t1.id = first_pair.min_id
     LEFT JOIN transfer_sum AS t2 -- to subtract the amount of a backward transfer
     ON t1.checking_peer = t2.checked_peer
     AND t1.checked_peer = t2.checking_peer
-    AND t1.checked_peer = t2.checking_peer;
+    AND t1.checked_peer = t2.checking_peer
+WHERE t1.points_amount - COALESCE(t2.points_amount, 0) <> 0;
 
 $$ LANGUAGE SQL;
 
@@ -61,15 +54,12 @@ OR REPLACE FUNCTION fnc_checked_tasks_xp() RETURNS TABLE(
     task VARCHAR,
     XP INTEGER
 ) AS $$
-SELECT
-    checks.peer AS peer,
+SELECT checks.peer AS peer,
     SPLIT_PART(checks.task, '_', 1) AS task,
     xp.xp_amount AS XP
-FROM
-    xp
+FROM xp
     JOIN checks ON xp. "check" = checks.id
-ORDER BY
-    peer,
+ORDER BY peer,
     task;
 
 $$ LANGUAGE SQL;
@@ -80,19 +70,13 @@ $$ LANGUAGE SQL;
 -- day
 CREATE
 OR REPLACE FUNCTION fnc_peers_all_day_in_campus(pdate DATE) RETURNS TABLE(peer VARCHAR) AS $$
-SELECT
-    peer
-FROM
-    time_tracking AS tt
-WHERE
-    DATE = pdate
+SELECT peer
+FROM time_tracking AS tt
+WHERE DATE = pdate
     AND state = 2
-GROUP BY
-    peer
-HAVING
-    COUNT(state) = 1
-ORDER BY
-    peer;
+GROUP BY peer
+HAVING COUNT(state) = 1
+ORDER BY peer;
 
 $$ LANGUAGE SQL;
 
@@ -102,28 +86,20 @@ $$ LANGUAGE SQL;
 -- TransferredPoints table
 CREATE
 OR REPLACE FUNCTION fnc_points_change() RETURNS TABLE(peer VARCHAR, points_change INTEGER) AS $$
-SELECT
-    peer,
+SELECT peer,
     SUM(points_amount) AS points_change
-FROM
-    (
-        SELECT
-            checking_peer AS peer,
+FROM (
+        SELECT checking_peer AS peer,
             points_amount
-        FROM
-            transferred_points
+        FROM transferred_points
         UNION
         ALL
-        SELECT
-            checked_peer AS peer,
+        SELECT checked_peer AS peer,
             points_amount * (-1)
-        FROM
-            transferred_points
+        FROM transferred_points
     ) AS d
-GROUP BY
-    peer
-ORDER BY
-    points_change DESC;
+GROUP BY peer
+ORDER BY points_change DESC;
 
 $$ LANGUAGE SQL;
 
@@ -133,27 +109,19 @@ $$ LANGUAGE SQL;
 -- table returned by the first function
 CREATE
 OR REPLACE FUNCTION fnc_points_change2() RETURNS TABLE(peer VARCHAR, points_change INTEGER) AS $$
-SELECT
-    peer,
+SELECT peer,
     SUM(points_amount) AS points_change
-FROM
-    (
-        SELECT
-            peer1 AS peer,
+FROM (
+        SELECT peer1 AS peer,
             points_amount
-        FROM
-            fnc_transferred_points()
+        FROM fnc_transferred_points()
         UNION
         ALL
-        SELECT
-            peer2 AS peer,
+        SELECT peer2 AS peer,
             points_amount * (-1)
-        FROM
-            fnc_transferred_points()
+        FROM fnc_transferred_points()
     ) AS d
-GROUP BY
-    peer
-ORDER BY
-    points_change DESC;
+GROUP BY peer
+ORDER BY points_change DESC;
 
 $$ LANGUAGE SQL;
